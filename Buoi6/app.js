d9fc25836ce01549172851b28f0b7e518316c5c6
khuedeptryyy <buiminhkhue98@gmail.com>
2021-04-23 18:10:27 +0700

    pt bac 2

--- Buoi6/app.js
+++ Buoi6/app.js
@@ -24,26 +24,28 @@
 // console.log(array);
 
 // Giai phuong trinh bac 2: ax^2 + bx + c = 0
-
-const a = 1;
-const b = 1;
-const c = -6;
-const x = -c / b;
-const delta = b * b - 4 * a * c;
-const x1 = ((-b + Math.sqrt(delta)) / 2) * a;
-const x2 = ((-b - Math.sqrt(delta)) / 2) * a;
+let a = prompt("Nhập a");
+let b = prompt("Nhập b");
+let c = prompt("Nhập c");
 
 if (a == 0) {
   if (b == 0) {
     if (c == 0) {
-      console.log("Phuong trinh vo so nghiem");
-    } else {
-      console.log("Phuong trinh vo nghiem");
+      alert("Phuong trinh vo so nghiem");
     }
   } else {
-    console.log("Phuong trinh co 1 nghiem duy nhat la: x=", x);
+    alert("Phuong trinh co nghiem duy nhat: " + -c / b);
   }
 } else {
-  console.log("Phuong trinh se co 2 nghiem kep:\nx1=", x1);
-  console.log("x2=", x2);
+  let delta = b * b - 4 * a * c;
+  if (delta > 0) {
+    let x1 = (-b + Math.sqrt(delta)) / (2 * a);
+    let x2 = (-b - Math.sqrt(delta)) / (2 * a);
+    alert("Phương trình có 2 nghiệm phân biệt:\n x1 = " + x1 + "\n x2 = " + x2);
+  } else if (delta == 0) {
+    let result = (-b / 2) * a;
+    alert("Phuong trinh co nghiem kep: x1 = x2 = " + result);
+  } else {
+    alert("Phuong trinh vo nghiem");
+  }
 }
